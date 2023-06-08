efb242618eef69040775ef94911d54ede911c906
ShaistaDaniya <shaista@gigandtake.com>
2023-06-08 16:36:30 +0530

    six

--- LogProject/index.js
+++ LogProject/index.js
@@ -1,9 +1,51 @@
-/**
- * @format
- */
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+interface HomeScreenProps {
+  navigation: any; 
+}
 
-AppRegistry.registerComponent(appName, () => App);
+function HomeScreen({ navigation }: HomeScreenProps) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+interface DetailsScreenProps {
+  navigation: any; // Replace 'any' with the appropriate type for navigation
+}
+
+function DetailsScreen({ navigation }: DetailsScreenProps) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
