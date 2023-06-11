506376ccda2bf158ef51bd1ad1815747d4949052
ShaistaDaniya <shaista@gigandtake.com>
2023-06-11 21:54:02 +0530

    14

--- LogProject/App.js
+++ LogProject/App.js
@@ -1,25 +1,111 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {AuthenticationScreen} from './src/AutheticationScreen';
-import {InputOTPScreen} from './src/InputOTPScreen';
+import React, { useState, useRef } from 'react';
+import { View, TextInput, Button, Alert, StyleSheet } from 'react-native';
 
-import {HomeScreen} from './src/HomeScreen';
+const PhoneNumberInput = ({ onNext }) => {
+  const [phoneNumber, setPhoneNumber] = useState('');
 
+  const handleNext = () => {
+    // Validation logic for phone number
+    if (phoneNumber.length === 10) {
+      onNext(phoneNumber);
+    } else {
+      Alert.alert('Invalid phone number', 'Please enter a valid 10-digit phone number');
+    }
+  };
 
-const Stack = createNativeStackNavigator();
-
-function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Authentication">
-        <Stack.Screen name="Authentication" component={AuthenticationScreen} />
-        <Stack.Screen name="InputOTP" component={InputOTPScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Phone Number"
+        keyboardType="numeric"
+        onChangeText={text => setPhoneNumber(text)}
+        value={phoneNumber}
+      />
+      <Button title="Next" onPress={handleNext} />
+    </View>
   );
-}
+};
+
+const OTPPage = ({ phoneNumber }) => {
+  const [otp, setOTP] = useState('');
+  const otpInputs = useRef([]);
+
+  const handleVerify = () => {
+    // Verification logic for OTP
+    if (otp.length === 4) {
+      Alert.alert('Success', 'OTP verified successfully');
+    } else {
+      Alert.alert('Invalid OTP', 'Please enter a valid 4-digit OTP');
+    }
+  };
+
+  const handleOTPChange = (index, value) => {
+    const newOTP = otp.split('');
+    newOTP[index] = value;
+    setOTP(newOTP.join(''));
+
+    // Move focus to the next input
+    if (value && index < otpInputs.current.length - 1) {
+      otpInputs.current[index + 1].focus();
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.otpContainer}>
+        {Array.from({ length: 4 }, (_, index) => (
+          <TextInput
+            key={index}
+            style={styles.otpInput}
+            keyboardType="numeric"
+            maxLength={1}
+            onChangeText={value => handleOTPChange(index, value)}
+            value={otp[index]}
+            ref={ref => (otpInputs.current[index] = ref)}
+          />
+        ))}
+      </View>
+      <Button title="Verify" onPress={handleVerify} />
+    </View>
+  );
+};
+
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleNext = (number) => {
+    setPhoneNumber(number);
+    setStep(2);
+  };
+
+  return (
+    <View style={styles.container}>
+      {step === 1 && <PhoneNumberInput onNext={handleNext} />}
+      {step === 2 && <OTPPage phoneNumber={phoneNumber} />}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  otpContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  otpInput: {
+    width: 50,
+    height: 50,
+    fontSize: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+});
 
 export default App;
